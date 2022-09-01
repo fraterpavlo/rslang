c11f344289415bc6b4bdd756924c5bf7d149bfc6
fraterpavlo <shukan1999@gmail.com>
2022-09-01 17:14:19 +0300

    feat: add common games files

---
+++ src/scripts/games/common/commonInterfaces.ts
@@ -0,0 +1,24 @@
+//AnimatedControl
+export interface IAnimatingClasses {
+  hide: string;
+  show: string;
+}
+
+
+//GameDataModel
+export interface IWordData {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  textExampleTranslate: string;
+  textMeaningTranslate: string;
+  wordTranslate: string;
+}
---
+++ src/scripts/games/common/gameDataModel.ts
@@ -0,0 +1,22 @@
+import { IWordData } from './commonInterfaces';
+
+export const baseUrl = 'https://rs-lang-rss-task.herokuapp.com';
+export const wordsUrl = `${baseUrl}/words`;
+// const users = `${baseUrl}/users`;
+
+export class GameDataModel {
+  constructor() {}
+
+  async getWords(group: number, page: number) {
+    const response = await fetch(`${wordsUrl}?page=${page}&group=${group}`);
+    const resBody: IWordData[] = await response.json();
+
+    return resBody;
+  }
+
+  async getSpecifiedWord(page: number, group: number) {
+    const response = await fetch(`${wordsUrl}?page=${page}&group=${group}`);
+    const resBody: IWordData[] = await response.json();
+    return resBody;
+  }
+}
